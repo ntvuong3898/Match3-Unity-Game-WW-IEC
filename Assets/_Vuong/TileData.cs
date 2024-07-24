1cf59576f9c00dca1b03ab26bdf9bb3bad319230
ntvuong3898 <75190299+ntvuong3898@users.noreply.github.com>
2024-07-24 22:59:35 +0700

    create + setup scriptable tile

---
+++ Assets/_Vuong/TileData.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "TileData", menuName = "ScriptableObjects/TileData")]
+public class TileData : ScriptableObject
+{
+    public GameObject Tile;
+}
